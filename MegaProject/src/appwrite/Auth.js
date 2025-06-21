d9db05467c0922c8d32a1548170000c77a999d93
Shivaji <shivajinani2004@gmail.com>
2025-06-21 11:40:35 +0530

    authorisation

--- MegaProject/src/appwrite/Auth.js
+++ MegaProject/src/appwrite/Auth.js
@@ -1,2 +1,60 @@
 import conf from "../conf/conf";
 
+import {Client, Account, ID} from "appwrite"
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+
+        account = new Account(this.client);
+    }
+
+    async CreateAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if(userAccount) {
+
+            }
+            else{
+                return userAccount;
+            }
+        } 
+        catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email,password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        }
+        catch(error) {
+            throw error;
+        }
+    }
+
+    async logout () {
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
